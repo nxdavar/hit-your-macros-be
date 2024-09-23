882f28847fe4e29635bd97b263abe4fd1b085e61
nxdavar <nealrr01@gmail.com>
2024-09-22 20:58:17 -0400

    refactor restaurant query folder

--- db/queries/restaurant_queries.py
+++ db/queries/restaurant_queries.py
@@ -6,12 +6,26 @@ from dotenv import load_dotenv
 import os
 
 
+# internal imports:
+from db.models.restaurant import Restaurant
+from db.utils.db_session import get_db, get_engine
+
+
 load_dotenv()
 engine = create_engine(os.getenv("DATABASE_URL"))
 metadata = MetaData()
 
 
 restaurant_table = Table("restaurant", metadata, autoload_with=engine)
+
+
+def get_restaurant_id_one(session: Session, res_name: str):
+    result = (
+        session.query(Restaurant.restaurant_id)
+        .filter(Restaurant.name == res_name)
+        .scalar()
+    )
+    return result
 
 
 # Function to get restaurant_id based on restaurant name
