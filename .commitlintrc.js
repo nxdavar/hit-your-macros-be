5946004f08c7f46a959ec32ceac5dfc3a9b7c30e
kpatell <krishanpatel00@gmail.com>
2024-10-11 02:27:25 -0400

    chore: update to use pipenv

---
+++ .commitlintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert'],
+    ],
+    'subject-min-length': [2, 'always', 1],
+  },
+};
